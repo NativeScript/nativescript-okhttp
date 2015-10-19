119cb9c9c83e15349d48ed03b4d49efe99ded2aa
Kiril Stanoev <kiril.stanoev@telerik.com>
2015-10-19 13:00:00 +0300

    Improved readme

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# NativeScript OkHttp Plugin
+# NativeScript plugin for OkHttp
+
+[OkHttp] is an HTTP client that’s efficient by default:
+
+- HTTP/2 and SPDY support allows all requests to the same host to share a socket.
+- Connection pooling reduces request latency (if SPDY isn’t available).
+- Transparent GZIP shrinks download sizes.
+- Response caching avoids the network completely for repeat requests.
+
+# Installation
+
+Run the following command from the root of your project:
+
+```
+$ tns plugin add nativescript-okhttp
+```
+
+This command automatically installs the necessary files, as well as stores nativescript-okhttp as a dependency in your project's `package.json` file.
+
+# Usage
+
+To use the social share module you must first `require()` it. After you `require()` the module you have access to its APIs.
+
+``` js
+var okHttp = require("nativescript-okhttp");
+```
+
+# API
+
+### getString(url)
+
+The `getString()` method downloads a URL as a string.
+
+``` js
+var okHttp = require("nativescript-okhttp");
+function pageLoaded(args) {
+    var result = okHttp.getString("http://www.google.com/");
+    console.log(result);
+}
+exports.pageLoaded = pageLoaded;
+```
+### getJSON(url)
+
+The `getString()` method downloads a URL as a string.
+
+``` js
+var okHttp = require("nativescript-okhttp");
+function pageLoaded(args) {
+    var json = okHttp.getJSON("https://api.github.com/gists/c2a7c39532239ff261be");
+    console.log("JSON url: " + json.url);
+}
+exports.pageLoaded = pageLoaded;
+```
+[okHttp]: <http://square.github.io/okhttp/>
+
+
--- nativescript-okhttp.android.js
+++ nativescript-okhttp.android.js
@@ -12,14 +12,7 @@ module.exports = {
 		var request = new com.squareup.okhttp.Request.Builder().url(url).build();
 		var response = client.newCall(request).execute();
 		var result = response.body().string();
-        var json = utils.parseJSON(result);
-        
-		console.log("\n\n ======================================================= \n\n");
-		console.log("client: " + (client != null) + "\n\n");
-		console.log("request: " + (request != null) + "\n\n");
-		console.log("response: " + (response != null) + "\n\n");
-        console.log("json: " + json.id + "\n\n");
-		console.log("\n\n ======================================================= \n\n");
+        return result;
     },
     getJSON: function(url) {
 		
@@ -35,53 +28,3 @@ module.exports = {
         return json;
     }
 };
-
-// Public API
-
-// getString
-// getJSON<T>
-// getImage
-
-/*
-var httpRequest = require("http/http-request");
-global.moduleMerge(httpRequest, exports);
-function getString(arg) {
-    return new Promise(function (resolve, reject) {
-        httpRequest.request(typeof arg === "string" ? { url: arg, method: "GET" } : arg)
-            .then(function (r) {
-            try {
-                var str = r.content.toString();
-                resolve(str);
-            }
-            catch (e) {
-                reject(e);
-            }
-        }, function (e) { return reject(e); });
-    });
-}
-exports.getString = getString;
-function getJSON(arg) {
-    return new Promise(function (resolve, reject) {
-        httpRequest.request(typeof arg === "string" ? { url: arg, method: "GET" } : arg)
-            .then(function (r) {
-            try {
-                var json = r.content.toJSON();
-                resolve(json);
-            }
-            catch (e) {
-                reject(e);
-            }
-        }, function (e) { return reject(e); });
-    });
-}
-exports.getJSON = getJSON;
-function getImage(arg) {
-    return new Promise(function (resolve, reject) {
-        httpRequest.request(typeof arg === "string" ? { url: arg, method: "GET" } : arg)
-            .then(function (r) {
-            r.content.toImage().then(function (source) { return resolve(source); }, function (e) { return reject(e); });
-        }, function (e) { return reject(e); });
-    });
-}
-exports.getImage = getImage;
-*/
